486d56c517d03a1e42fdfefca4e43c2dd3d4ffa1
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-11 23:19:11 -0300

    se modifico el componente Navbar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,47 @@
 import { Link } from 'react-router-dom';
 import logoNav from '../../src/assets/logo-nav.png';
 import CartWidget from './CartWidget';
+import { useState } from 'react';
 
 const NavBar = () => {    
 
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleShow = () => {
+    setDropdown(!dropdown);
+  };
+
+  const handledisabled = () => {
+    setDropdown(false);
+  };
+
   return (
     <nav className='navbar-container'>
-        <Link to="/"><img src={logoNav} className='logo-nav' alt="logo" /></Link>
-        <ul className='nav-items'>
-            <li><Link to="/">inicio</Link></li>
+        <Link to="/coffeStars/"><img src={logoNav} className='logo-nav' alt="logo" /></Link>
+        <span 
+          class={`material-symbols-outlined home-icon-drop ${ dropdown && 'drop-disabled'}`}
+          onClick={handleShow}>menu</span>
+        <div 
+          className={`navbar-dropdown-container ${dropdown &&  'dropdown-active'}`}>
+          <div className='dropdown-wrapper'>
+            <span 
+              class="material-symbols-outlined"
+              onClick={handledisabled}>close</span>
+            <ul className='nav-items'>
+              <li><Link to="/coffeStars/">inicio</Link></li>
+              <li><Link to="/products">menu stars</Link></li>
+              <li><Link to="/suscribeCoffe+">suscribirse +</Link></li>    
+            </ul>
+          </div>
+        </div>
+        <div className='navbar-desktop-container'>
+          <ul className='nav-items'>
+            <li><Link to="/coffeStars/">inicio</Link></li>
             <li><Link to="/products">menu stars</Link></li>
             <li><Link to="/suscribeCoffe+">suscribirse +</Link></li>    
             <li><CartWidget/></li>
-        </ul>
+          </ul>
+        </div>
     </nav>
   
   )
