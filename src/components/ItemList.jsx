d25142f1cb7905ce7caa5af8fb2f385d90c81659
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-08-11 23:17:59 -0300

    se modifico el componente ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,11 +2,9 @@ import Item from './Item';
 
 const ItemList = ( {products} ) => {
   return (
-    <div>
-        <div className="coffes-all">
-            {products.map((prod) => <Item key={prod.id} prod={prod}/>)}
-        </div>
-    </div>
+      <div className="coffes-all">
+        {products.map((prod) => <Item key={prod.id} prod={prod}/>)}
+      </div>
   )
 }
 
